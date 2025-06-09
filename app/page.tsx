fbec4e520fc5d2985789188c23dfe45cddfc7500
Berke Güleryüz <berkeguleryuz@gmail.com>
2025-06-09 21:06:17 +0200

    v.0

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,21 @@ import { FaXTwitter } from "react-icons/fa6";
 
 export default function Home() {
   return (
-    <main>
+    <main className="relative bg-[#0B0D10] flex flex-col items-center justify-center w-screen h-screen">
+      <div className="absolute top-5 right-5">
+        <Link
+          href="https://x.com/Nyxara_N1"
+          target="_blank"
+          rel="noopener noreferrer">
+          <FaXTwitter size={48} className="text-white hover:text-orange-300 transition-colors duration-300" />
+        </Link>
+      </div>
       <Link
         href="https://x.com/Nyxara_N1"
-        className="relative flex flex-col items-center justify-center h-screen w-screen bg-[#0B0D10]"
         target="_blank"
         rel="noopener noreferrer">
         <Image
-          src="/comingsoon.jpg"
+          src="/comingsoon.png"
           alt="Nyxara N1"
           quality={100}
           unoptimized
@@ -29,14 +36,6 @@ export default function Home() {
           onDrag={(e) => e.preventDefault()}
           onDrop={(e) => e.preventDefault()}
         />
-        <div className="text-white text-center text-sm">
-          <Link
-            href="https://x.com/Nyxara_N1"
-            target="_blank"
-            rel="noopener noreferrer">
-            <FaXTwitter size={48} />
-          </Link>
-        </div>
       </Link>
     </main>
   );
